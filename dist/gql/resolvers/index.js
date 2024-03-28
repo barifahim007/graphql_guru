3fc2c24a7b47ae92859af56df994ffcbe64ad3e6
fahim007 <ehsanulbarifahim7@gmail.com>
2024-03-29 01:22:48 +0600

    graphql done

---
+++ dist/gql/resolvers/index.js
@@ -0,0 +1,37 @@
+import { db } from "../../db.js";
+export const resolvers = {
+    Query: {
+        products: () => db.products,
+        product: (parent, args, context) => {
+            const result = db.products.find((product) => product.id === args.productId);
+            return result;
+        },
+        categories: () => db.categories,
+        category: (parent, args, context) => {
+            const result = db.categories.find((category) => category.id === args.categoryId);
+            return result;
+        },
+        reviews: () => db.reviews,
+        review: (parent, args, context) => {
+            const result = db.reviews.find((review) => review.id === args.reviewId);
+            return result;
+        },
+    },
+    Product: {
+        categoryId: (parent, args, context) => {
+            const result = db.categories.find((cate) => cate.id === parent.categoryId);
+            return result;
+        },
+        reviews: (parent, args, context) => {
+            const result = db.reviews.filter((review) => review.productId === parent.id);
+            console.log(result);
+            return result;
+        },
+    },
+    Category: {
+        productId: (parent, args, context) => {
+            const result = db.products.filter((product) => product.categoryId === parent.id);
+            return result;
+        },
+    },
+};
--- src/gql/resolvers/index.ts
+++ src/gql/resolvers/index.ts
@@ -1,3 +1,49 @@
+import { db } from "../../db.js";
+
 export const resolvers = {
-  Query: {},
+  Query: {
+    products: () => db.products,
+    product: (parent: any, args: { productId: string }, context: any) => {
+      const result = db.products.find(
+        (product) => product.id === args.productId
+      );
+      return result;
+    },
+    categories: () => db.categories,
+    category: (parent, args, context) => {
+      const result = db.categories.find(
+        (category) => category.id === args.categoryId
+      );
+      return result;
+    },
+    reviews: () => db.reviews,
+    review: (parent, args, context) => {
+      const result = db.reviews.find((review) => review.id === args.reviewId);
+      return result;
+    },
+  },
+  Product: {
+    categoryId: (parent, args, context) => {
+      const result = db.categories.find(
+        (cate) => cate.id === parent.categoryId
+      );
+      return result;
+    },
+    reviews: (parent, args, context) => {
+      const result = db.reviews.filter(
+        (review) => review.productId === parent.id
+      );
+      console.log(result);
+      return result;
+    },
+  },
+
+  Category: {
+    productId: (parent, args, context) => {
+      const result = db.products.filter(
+        (product) => product.categoryId === parent.id
+      );
+      return result;
+    },
+  },
 };
